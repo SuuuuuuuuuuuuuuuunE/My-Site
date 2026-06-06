28ace3e2f59f6458bb019f11347ea29a1de96a73
SuuuuuuuuuuuuuuuunE <funnysunny2010@gmail.com>
2026-06-06 16:31:55 -0700

    Added Games Tab
    - Testing Turbowarp Game (Unfinished)
    - Javascript Fractal Explorer (Finished)
    > Topbar added using JS in root web pages, still figuring out how to add "../" before file paths in non-root web pages.

---
+++ topbar_setup.js
@@ -0,0 +1,12 @@
+document.getElementById('topbar').innerHTML =
+`
+<a class="topbuttonimage" href="index.html">
+    <img src="resources/Squarun Square.svg" alt="Happy Square from Squarun" width="50" style="position:relative; top:-50%;">
+</a>
+<!--<a class="topbutton" href="https://pmss.sd43.bc.ca/sunnyx/"> Edublog </a>-->
+<p> My Games: </p>
+<a class="topbutton" href="scratch.html"> Scratch </a>
+<a class="topbutton" href="python.html"> Python </a>
+<a class="topbutton" href="unity.html"> Unity </a>
+<a class="topbutton" href="games.html"> Games </a>
+`
